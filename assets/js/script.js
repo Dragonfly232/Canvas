789763d34ad086e13bd0dedda2c136bd5af109d6
Dragonfly232 <RonjiaNellemann1@gmail.com>
2022-03-11 09:45:45 +0000

    border added to canvas

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@ const canvas = document.getElementById("playGround");
 const context = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+
+var canvas = document.getElementById("playGround");
+
+    if (canvas.getContext) {
+        var ctx = getContext("2d");
+    } else {
+        
+    } 
